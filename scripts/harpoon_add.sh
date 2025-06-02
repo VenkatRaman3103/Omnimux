9d81d448fa2bd7bd1e2f28a41e381c05f1d9003d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-02 18:31:10 +0530

    udated harpoon add script to print a messge for indicationOC

--- scripts/harpoon_add.sh
+++ scripts/harpoon_add.sh
@@ -4,3 +4,4 @@ target=$(tmux display -p '#{session_name}:#{window_index}')
 
 grep -Fxq "$target" "$FILE" 2>/dev/null || echo "$target" >> "$FILE"
 
+tmux display-message "Session: #S, Window: #W, Pane: #P is added to the list"
